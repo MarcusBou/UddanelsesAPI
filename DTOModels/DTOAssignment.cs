2db74edff885e6d4668051134ff09e58ac88974e
MarcusBou <60425915+MarcusBou@users.noreply.github.com>
2023-11-27 22:49:58 +0100

    Test

---
+++ DTOModels/DTOAssignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOModels
+{
+    public class DTOAssignment
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public string Name { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ DTOModels/DTOSubject.cs
@@ -0,0 +1,8 @@
+﻿namespace DTOModels
+{
+    public class DTOSubject
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
